5e1a6adf574c428891f8ce1fb1ebc175cf13677a
liyanbo <liyanbo.monster@bytedance.com>
2018-06-27 09:38:21 +0800

    Add local files

---
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -0,0 +1,83 @@
+package com.monster.leetcode_practices;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class LeetcodeTest {
+    @Test
+    public void main() {
+        int n = 25;
+//        trailingZeroes(25);
+//        rotate(new int[]{1,2,3,4,5,6,7}, 3);
+//        hammingWeight(  4294967295);
+        reverseBits(43261596);
+    }
+
+    public int trailingZeroes(int n) {
+        String res = "" + helper(n);
+        int result = 0;
+        for (int j = res.length()-1;j>=0;j--) {
+            if (res.charAt(j) == '0') {
+                result++;
+            } else {
+                return result;
+            }
+        }
+        return result;
+
+    }
+    private double helper(int n) {
+        if (n == 1) {
+            return n;
+        } else {
+            return n*helper(n-1);
+        }
+    }
+
+
+    public void rotate(int[] nums, int k) {
+        int temp = 0;
+        int temp2 = 0;
+        int times = nums.length / k;
+        for (int i = 0; i< times; i++) {
+            temp = nums[i];
+            for (int j = i; j<nums.length;) {
+                temp2 = nums[(j+k)%nums.length];
+                nums[(j+k)%nums.length] = temp;
+                temp = temp2;
+                j += k;
+                if ( j > nums.length) {
+                    nums[i] = temp;
+                }
+            }
+        }
+
+    }
+
+    public int hammingWeight(int n) {
+        String n2 = Integer.toBinaryString(n);
+        int res = 0;
+        for (int i = 0; i<n2.length();i++) {
+            if (n2.charAt(i) == '1') {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    public int reverseBits(int n) {
+        String rs = Integer.toBinaryString(n);
+        int add = 32 - rs.length();
+        for (int i = 0 ;i<add;i++) {
+            rs = "0" + rs;
+        }
+        rs = (new StringBuilder(rs)).reverse().toString();
+        return (int)Long.parseLong(rs, 2);
+    }
+}
