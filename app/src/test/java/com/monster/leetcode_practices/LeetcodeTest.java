1d46fed268bb70ca40e9d3824faeb3f30e36a20a
liyanbo <liyanbo.monster@bytedance.com>
2018-07-03 09:52:40 +0800

    Finish reverseList

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -21,7 +22,13 @@ public class LeetcodeTest {
 //        hammingWeight(  4294967295);
 //        reverseBits(43261596);
 //        System.out.print(isHappy(3));
-        System.out.print(isIsomorphic("foo", "bar"));
+//        System.out.print(isIsomorphic("foo", "bar"));
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+        reverseList(listNode);
     }
 
     public int trailingZeroes(int n) {
@@ -179,5 +186,29 @@ public class LeetcodeTest {
             }
         }
         return true;
+    }
+
+    // 6.2%
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        Stack<ListNode> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+        ListNode reverseNode = null;
+        if (!stack.empty()) {
+            head = stack.peek();
+        }
+        while (!stack.empty()){
+            reverseNode = stack.pop();
+            if (!stack.empty()) {
+                reverseNode.next = stack.peek();
+            }
+        }
+        reverseNode.next = null;
+        return head;
     }
 }
