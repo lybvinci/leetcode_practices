d4f4a3d4bd473f0f6f900db871ab501eaec3ee2a
liyanbo <liyanbo.monster@bytedance.com>
2018-07-18 09:37:34 +0800

    Finish lowestCommonAncestor

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -556,4 +556,20 @@ public class LeetcodeTest {
         }
         return true;
     }
+
+    // 21.87%
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        if ((root.val > p.val && root.val < q.val) ||
+                (root.val <p.val && root.val > q.val)) {
+            return root;
+        }
+        if (root.val > p.val && root.val > q.val ) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+    }
 }
