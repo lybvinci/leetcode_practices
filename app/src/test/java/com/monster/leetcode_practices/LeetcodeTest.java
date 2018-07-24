e3dee4c8328ad25f66e9dfd6ea0dbd31c18439d9
liyanbo <liyanbo.monster@bytedance.com>
2018-07-24 09:30:36 +0800

    Finish addDigits

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -637,4 +637,17 @@ public class LeetcodeTest {
             helperBinaryTreePaths(root.right, path);
         }
     }
+
+    //79.12%
+    public int addDigits(int num) {
+        while(num > 9) {
+            int sum = 0;
+            while(num != 0) {
+                sum += num % 10 ;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
 }
