4b563e1d48f51c634902c36d3d5c0ef95d472ca0
lybvinci <lybvinci@gmail.com>
2018-07-27 09:38:18 +0800

    Finish missingNumber

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -42,11 +42,12 @@ public class LeetcodeTest {
 //        myStack.pop();
 //        myStack.pop();
 //        moveZeroes(new int[]{0,1,0,3,12});
-        int i = -2147483648;
-        long ii = (long)i;
-        long num = i >0?i:-i;
-        System.out.print(num);
+//        int i = -2147483648;
+//        long ii = (long)i;
+//        long num = i >0?i:-i;
+//        System.out.print(num);
 //        isUgly(-2147483648);
+        firstBadVersion(5);
     }
 
     public int trailingZeroes(int n) {
@@ -706,5 +707,24 @@ public class LeetcodeTest {
             }
         }
         return 0;
+    }
+
+    // 95.62%
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        while (start < end) {
+            int mid = start + (end - start)/2;
+            if (isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+    // for test
+    private boolean isBadVersion(int n) {
+        return n==4;
     }
 }
