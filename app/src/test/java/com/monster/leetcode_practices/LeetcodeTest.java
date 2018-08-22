803253edeb3a97f6313c579c2cf2a3497a2e6ce8
lybvinci <lybvinci@gmail.com>
2018-08-22 09:52:52 +0800

    Finish 404.sumOfLeftLeaves

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -1171,4 +1171,24 @@ public class LeetcodeTest {
         }
         return third == Long.MIN_VALUE ? (int)first : (int)third;
     }
+
+    //84.91%
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                sum += root.left.val;
+            } else {
+                sum += sumOfLeftLeaves(root.left);
+            }
+        }
+        if (root.right != null) {
+            sum += sumOfLeftLeaves(root.right);
+        }
+
+        return sum;
+    }
 }
