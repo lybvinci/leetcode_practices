e2a60a15b8f711b16b4358649a13c6db51de0fa7
lybvinci <lybvinci@gmail.com>
2018-08-24 09:34:01 +0800

    Finish 400.findNthDigit

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -1210,4 +1210,23 @@ public class LeetcodeTest {
         }
         return result * 2 < s.length() ? result*2 + 1 : result * 2;
     }
+
+    // 40.12%
+    public int findNthDigit(int n) {
+        int digitType = 1;
+        long digitNum = 9;
+        //定位到是几位数
+        while(n > digitNum*digitType){
+            n -= (int) digitNum*digitType ;
+            digitType++;
+            digitNum*=10;
+        }
+        //定位到是这些几位数里面的第几个的第几位
+        int indexInSubRange = (n -1) / digitType;
+        int indexInNum = (n -1) % digitType;
+        //还原数字
+        int num = (int)Math.pow(10,digitType - 1) + indexInSubRange ;
+        int result = Integer.parseInt((""+num).charAt(indexInNum)+"");
+        return result;
+    }
 }
