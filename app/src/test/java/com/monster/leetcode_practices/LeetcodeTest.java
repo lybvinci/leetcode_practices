b041bfb282af988e0da60ef4db69eec631f4a0f0
lybvinci <lybvinci@gmail.com>
2018-08-29 09:12:45 +0800

    Finish 455.findContentChildren

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -1281,4 +1281,22 @@ public class LeetcodeTest {
             return sum;
         }
     }
+
+    //61.72%
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int result = 0;
+        for (int i = 0 ,j = 0; i < g.length && j <s.length ;) {
+            if (s[j] >= g[i]) {
+                result++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return result;
+
+    }
 }
