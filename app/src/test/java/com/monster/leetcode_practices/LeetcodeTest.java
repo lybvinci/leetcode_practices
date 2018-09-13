d96cde0b7e8c1df24d0701c20a1e7bc04cc557ce
lybvinci <lybvinci@gmail.com>
2018-09-13 09:28:42 +0800

    Finish 496.nextGreaterElement

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -1530,4 +1530,29 @@ public class LeetcodeTest {
         helperFindMode(root.right, map);
     }
 
+    //16.70%
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            boolean start = false;
+            boolean hasGT = false;
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    start = true;
+                    continue;
+                }
+                if (start && nums1[i] < nums2[j]) {
+                    result[i] = nums2[j];
+                    hasGT = true;
+                    break;
+                }
+
+            }
+            if (!hasGT) {
+                result[i] = -1;
+            }
+        }
+        return result;
+    }
+
 }
