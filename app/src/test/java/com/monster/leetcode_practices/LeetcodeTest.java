556f2b47997da2ddaa401d9e4d3e9b0af4814091
lybvinci <lybvinci@gmail.com>
2018-10-26 09:40:30 +0800

    Finish 661.imageSmoother

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -2288,4 +2288,51 @@ public class LeetcodeTest {
         return container[0] == 0 && container[1] == 0;
     }
 
+    //10.26%
+    public int[][] imageSmoother(int[][] M) {
+        int[][] res = new int[M.length][M[0].length];
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M[0].length;j++) {
+                int sum = 0;
+                int num = 0;
+                if (i - 1 >= 0) {
+                    if (j - 1 >= 0) {
+                        sum += M[i-1][j-1];
+                        num++;
+                    }
+                    sum += M[i-1][j];
+                    num++;
+                    if (j + 1 < M[0].length) {
+                        sum += M[i-1][j+1];
+                        num++;
+                    }
+                }
+                sum += M[i][j];
+                num++;
+                if (j -1 >= 0) {
+                    sum += M[i][j-1];
+                    num++;
+                }
+                if (j + 1 < M[0].length) {
+                    sum += M[i][j+1];
+                    num++;
+                }
+                if (i + 1 < M.length) {
+                    if (j -1 >= 0) {
+                        sum += M[i+1][j-1];
+                        num++;
+                    }
+                    sum += M[i+1][j];
+                    num++;
+                    if (j + 1 < M[0].length) {
+                        sum += M[i+1][j+1];
+                        num++;
+                    }
+                }
+                res[i][j] = sum / num;
+            }
+        }
+        return res;
+    }
+
 }
