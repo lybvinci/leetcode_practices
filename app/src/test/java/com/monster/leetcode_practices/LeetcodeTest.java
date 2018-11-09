baafb53f8888dd8100771e736765e3bf2374487b
lybvinci <lybvinci@gmail.com>
2018-11-09 09:24:11 +0800

    Finish 700.searchBST

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -2575,4 +2575,19 @@ public class LeetcodeTest {
         }
         return result;
     }
+
+    //100.00%
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
 }
