b0e443bb5b41cc2bebedf27cd679dbc595c955c4
lybvinci <lybvinci@gmail.com>
2018-11-26 09:11:55 +0800

    Finish 812.largestTriangleArea

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -2840,4 +2840,17 @@ public class LeetcodeTest {
         }
         return res;
     }
+
+    //66.27%
+    public double largestTriangleArea(int[][] points) {
+        double area = 0;
+        for (int[] a : points) {
+            for (int[] b : points) {
+                for (int[] c : points) {
+                    area = Math.max(area, 0.5 * Math.abs(a[0] * b[1] + b[0] * c[1] + c[0] * a[1] - a[0] * c[1] - b[0] * a[1] - c[0] * b[1]));
+                }
+            }
+        }
+        return area;
+    }
 }
