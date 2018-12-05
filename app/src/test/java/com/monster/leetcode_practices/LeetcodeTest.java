cba11b99227c2d17431c5ae65bb0b2b23836805c
lybvinci <lybvinci@gmail.com>
2018-12-05 09:45:36 +0800

    Finish 832.flipAndInvertImage

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -2957,4 +2957,15 @@ public class LeetcodeTest {
         }
         return result;
     }
+
+    // 97.46%
+    public int[][] flipAndInvertImage(int[][] A) {
+        int[][] result = new int[A.length][A[0].length];
+        for (int i = 0 ; i < A.length;i++) {
+            for (int j = A[i].length - 1; j >= 0 ;j--) {
+                result[i][A[i].length - 1 - j] = A[i][j] == 1 ? 0: 1;
+            }
+        }
+        return result;
+    }
 }
