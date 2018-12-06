da3310625098718f72d8e0098f62373bb75b5473
lybvinci <lybvinci@gmail.com>
2018-12-06 09:29:43 +0800

    Finish 836.isRectangleOverlap

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -2968,4 +2968,9 @@ public class LeetcodeTest {
         }
         return result;
     }
+
+    //88.46%
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return rec2[2] > rec1[0] && rec2[0] < rec1[2] && rec2[3] > rec1[1] && rec2[1] < rec1[3];
+    }
 }
