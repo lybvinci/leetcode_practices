08c6e96aab5c8eef8735a3040cc2f5d8451bf6db
lybvinci <lybvinci@gmail.com>
2019-01-04 09:29:58 +0800

    Finish 905.sortArrayByParity

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -3323,4 +3323,19 @@ public class LeetcodeTest {
         }
     }
 
+    //86.52%
+    public int[] sortArrayByParity(int[] A) {
+        int[] res = new int[A.length];
+        int start = 0;
+        int end = A.length - 1;
+        for (int i : A) {
+            if (i % 2 == 0) {
+                res[start++] = i;
+            } else {
+                res[end--] = i;
+            }
+        }
+        return res;
+    }
+
 }
