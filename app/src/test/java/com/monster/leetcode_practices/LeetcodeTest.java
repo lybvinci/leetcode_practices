f85e75ff5c0f61ec8c39755debae186c760e2b8d
lybvinci <lybvinci@gmail.com>
2019-03-06 10:09:40 +0800

    Finish 36.isValidSudoku

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -4064,5 +4064,61 @@ public class LeetcodeTest {
         return new int[]{start, end};
     }
 
+    //31.82%
+    public boolean isValidSudoku(char[][] board) {
+        for (int i =0; i < board.length ; i++) {
+            HashSet<Character> set = new HashSet<>();
+            for (int j = 0; j < board[0].length ; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (!set.contains(board[i][j])) {
+                    set.add(board[i][j]);
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        for (int i =0; i < board.length ; i++) {
+            HashSet<Character> set = new HashSet<>();
+            for (int j = 0; j < board[0].length ; j++) {
+                if (board[j][i] == '.') {
+                    continue;
+                }
+                if (!set.contains(board[j][i])) {
+                    set.add(board[j][i]);
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        for (int i =0; i < board.length ;) {
+            int endRow = i + 3;
+            for (int j = 0; j < board.length;) {
+                int endCol = j + 3;
+                HashSet<Character> set = new HashSet<>();
+                for (int k = i; k < endRow; k++) {
+                    for (int l = j; l < endCol; l++) {
+                        if (board[k][l] == '.') {
+                            continue;
+                        }
+                        if (!set.contains(board[k][l])) {
+                            set.add(board[k][l]);
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+                j = endCol;
+
+            }
+            i = endRow;
+        }
+
+        return true;
+    }
+
 
 }
