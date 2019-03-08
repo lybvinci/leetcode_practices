378b5f46b72e82546699e9cdd844197b00caf564
lybvinci <lybvinci@gmail.com>
2019-03-08 09:48:59 +0800

    Finish 43.multiply

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -111,7 +111,8 @@ public class LeetcodeTest {
 //        fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0);
 //        divide(-2147483648 ,1);
 //        nextPermutation(new int[]{1,2,3});
-        combinationSum(new int[]{2,3,6,7}, 7);
+//        combinationSum(new int[]{2,3,6,7}, 7);
+        multiply("123", "456");
     }
 
 
@@ -4144,6 +4145,30 @@ public class LeetcodeTest {
         }
 
 
+    }
+
+    //90.17%
+    public String multiply(String num1, String num2) {
+        if ("0".equals(num1) || "0".equals(num2)) {
+            return "0";
+        }
+        int len1 = num1.length(), len2 = num2.length();
+        int A[] = new int[len1 + len2];
+        for(int i = len1-1; i >= 0; i--){
+            for(int j = len2-1; j >= 0; j--){
+                A[len1+len2-2-i-j] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+            }
+        }
+
+        String result = "";
+        for(int i = 0; i < len1+len2-1; i++){
+            A[i+1] += A[i]/10;
+            A[i] %= 10;
+            result = (char)(A[i] + '0') + result;
+        }
+        result = (0 == A[len1+len2-1] ? "" : (char)(A[len1+len2-1]+'0')) + result;
+
+        return result;
     }
 
 
