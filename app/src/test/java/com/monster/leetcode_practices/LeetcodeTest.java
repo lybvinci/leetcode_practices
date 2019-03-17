3bbcd0ebe6a4c2889e10cfd8acaeabc2009939cd
lybvinci <lybvinci@gmail.com>
2019-03-17 10:22:58 +0800

    Finish 5.longestPalindrome2

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -4277,4 +4277,26 @@ public class LeetcodeTest {
         }
         return n < 0 ? 1 / p / x : p;
     }
+
+    //40.41%
+    public String longestPalindrome2(String s) {
+        if (s.isEmpty()) {
+            return s;
+        }
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int left = 0;
+        int right = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i][i] = true;
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) &&( j-i<3||dp[i+1][j-1]);//小于3是因为aba一定是回文
+                if(dp[i][j]&&right-left<j-i){
+                    left=i;
+                    right=j;
+                }
+            }
+        }
+        return s.substring(left,right+1);
+    }
 }
