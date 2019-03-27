5134f2909da5909a4154d259466336cbad4c52df
lybvinci <lybvinci@gmail.com>
2019-03-27 10:00:23 +0800

    Finish 63.uniquePathsWithObstacles

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -4495,4 +4495,24 @@ public class LeetcodeTest {
         }
         return dp[m - 1][n - 1];
     }
+
+    //94.72%
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] res = new int[m+1][n+1];
+        if (obstacleGrid[0][0] == 0) {
+            res[1][1] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (obstacleGrid[i-1][j-1] == 0) {
+                    res[i][j] += res[i-1][j];
+                    res[i][j] += res[i][j-1];
+                }
+            }
+        }
+        return res[m][n];
+
+    }
 }
