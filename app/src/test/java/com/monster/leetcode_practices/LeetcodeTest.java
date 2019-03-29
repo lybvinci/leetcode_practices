8cfd92ea0ca9515eae1c3f2c331451760af678d0
lybvinci <lybvinci@gmail.com>
2019-03-29 10:04:39 +0800

    Finish 73.setZeroes

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -4539,4 +4539,29 @@ public class LeetcodeTest {
         }
         return result.length() > 0 ? result : "/";
     }
+
+    //87.83%
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int m0 = 1;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) m0 = 0;
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (m0 == 0) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 }
