095de490da6627ee38b29e0c6286b0bac02c7fc0
lybvinci <lybvinci@gmail.com>
2019-03-31 10:20:45 +0800

    Finish 74.searchMatrix

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -4564,4 +4564,28 @@ public class LeetcodeTest {
             }
         }
     }
+
+    //100.00%
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return  false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m; i++ ) {
+            int start = matrix[i][0];
+            int end = matrix[i][n -1];
+            if (target < start) {
+                return false;
+            } else if (target >= start && target <= end) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == target) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        return false;
+    }
 }
