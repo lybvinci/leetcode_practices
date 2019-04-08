ef03b532462037eec0c907512d4746dfe7e91524
lybvinci <lybvinci@gmail.com>
2019-04-08 10:16:07 +0800

    Finish 79.word exist

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -4648,4 +4648,58 @@ public class LeetcodeTest {
             item.removeLast();
         }
     }
+
+    //3.87%
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+        for (int i = 0; i < row; i ++) {
+            for (int j = 0 ; j < col; j++) {
+                boolean[][] visit = new boolean[row][col];
+                if (existHelper(board, i, j, word, 0, visit)) {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+
+    public boolean existHelper(char[][] board, int i, int j, String word, int wordIndex, boolean[][] visit) {
+        if (wordIndex == word.length()) {
+            return true;
+        }
+        if (i >= board.length || j >= board[0].length ||
+                i < 0 || j < 0 || visit[i][j]) {
+            return false;
+        }
+        char t = word.charAt(wordIndex);
+        if (board[i][j] == t) {
+            visit[i][j] = true;
+            boolean result = existHelper(board, i -1, j, word, wordIndex+1, visit);
+            if (result) {
+                return result;
+            }
+            result = existHelper(board, i + 1, j,word, wordIndex+1, visit);
+            if (result) {
+                return result;
+            }
+            result = existHelper(board, i, j+1, word, wordIndex+1, visit);
+            if (result) {
+                return result;
+            }
+            result = existHelper(board, i, j - 1, word, wordIndex+1, visit);
+            if (result) {
+                return result;
+            }
+            visit[i][j] = false;
+            return false;
+        } else {
+            return false;
+        }
+    }
+
+
+
 }
