7c3c667d55e67dfb21f69857cef54b9724fc7414
lybvinci <lybvinci@gmail.com>
2019-05-14 10:02:56 +0800

    Finish 114.flatten

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -5301,4 +5301,22 @@ public class LeetcodeTest {
         }
     }
 
+    //97.42%
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = null;
+        while (root.right != null) {
+            root = root.right;
+        }
+        root.right = temp;
+
+    }
+
+
 }
