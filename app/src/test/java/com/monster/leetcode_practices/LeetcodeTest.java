326162f84ab93a7f81625f820272abde366bb6db
lybvinci <lybvinci@gmail.com>
2019-05-15 09:53:35 +0800

    Finish 120.minimumTOtal

--- app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
+++ app/src/test/java/com/monster/leetcode_practices/LeetcodeTest.java
@@ -5318,5 +5318,15 @@ public class LeetcodeTest {
 
     }
 
+    //21.88%
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); i++) {
+                triangle.get(i).set(j, triangle.get(i).get(j) + Math.min(triangle.get(i+1).get(j), triangle.get(i+1).get(j+1)));
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+
 
 }
