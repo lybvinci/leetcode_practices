25f44ca7d13f492f890b15fd989713f0a5ef4575
liyanbo.monster <liyanbo.monster@bytedance.com>
2019-07-11 10:04:55 +0800

    Finish 230.kthSmallest

---
+++ src/com/monster/learn/KthSmallest.java
@@ -0,0 +1,41 @@
+package com.monster.learn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//6.96%
+public class KthSmallest {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> nums = new ArrayList<>();
+        helper(root, nums, k);        
+        if (nums.size() >= k) {
+            return nums.get(k - 1);
+        } else {
+            return -1;
+        }
+        
+    }
+
+    private boolean helper(TreeNode root, List<Integer> nums, int k) {
+        if (nums.size() >= k) {
+            return true;
+        }
+        if (root == null) {
+            return false;
+        }
+        boolean ret = false;
+        ret = helper(root.left, nums, k);
+        if (ret) {
+            return true;
+        }
+        nums.add(root.val);
+        if (nums.size() >= k) {
+            return true;
+        }
+        ret = helper(root.right, nums, k);
+        if (ret) {
+            return true;
+        }
+        return false;
+    }
+}
