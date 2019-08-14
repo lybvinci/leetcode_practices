d9877400dd929d4d7d165ecfe0e55c58a795823d
liyanbo.monster <liyanbo.monster@bytedance.com>
2019-08-14 10:25:59 +0800

    Refactor 322.coinChange

--- src/com/monster/learn/CoinChange.java
+++ src/com/monster/learn/CoinChange.java
@@ -1,8 +1,21 @@
 package com.monster.learn;
 
+import java.util.Arrays;
+
 //81.97%
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+        /*
         if (amount == 0) return 0;
         int[] dp = new int[amount + 1];
         for (int i = 1; i <= amount; i++) {
@@ -14,5 +27,6 @@ public class CoinChange {
             }
         }
         return dp[amount] == 999999 ? -1 : dp[amount];
+        */
     }
 }
