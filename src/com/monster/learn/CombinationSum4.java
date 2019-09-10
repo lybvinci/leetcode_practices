00071c252d986816986db5411014e06b7bbbed68
liyanbo.monster <liyanbo.monster@bytedance.com>
2019-09-10 10:21:51 +0800

    Finish 377.combinationSum4

---
+++ src/com/monster/learn/CombinationSum4.java
@@ -0,0 +1,23 @@
+package com.monster.learn;
+
+import java.util.Arrays;
+
+//88.11%
+public class CombinationSum4 {
+    public int combinationSum4(int[] nums, int target) {
+        Arrays.sort(nums);
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i >= nums[j]) {
+                    dp[i] += dp[i - nums[j]];
+                } else {
+                    break;
+                }
+            }
+        }
+        return dp[target];
+    }
+    
+}
