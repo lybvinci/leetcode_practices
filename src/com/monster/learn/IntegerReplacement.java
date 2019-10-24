8fa4f80a3aead02b425b682622298ffd80c85cc8
liyanbo.monster <liyanbo.monster@bytedance.com>
2019-10-24 10:39:27 +0800

    Finish 397.integerReplacement

---
+++ src/com/monster/learn/IntegerReplacement.java
@@ -0,0 +1,24 @@
+package com.monster.learn;
+
+//100%
+public class IntegerReplacement {
+    public int integerReplacement(int n) {
+        if (n == Integer.MAX_VALUE) {
+            return 32;
+        }
+        int count = 0;
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                if ((n+1) % 4 == 0 && (n - 1 != 2)) {
+                    n++;
+                } else {
+                    n--;
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+}
