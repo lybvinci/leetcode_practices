cfe21e5597f105af42db5ae9dd7d84b94d9ae597
liyanbo.monster <liyanbo.monster@bytedance.com>
2019-12-02 10:03:43 +0800

    Finish 509.fib

---
+++ src/com/monster/learn/Fib.java
@@ -0,0 +1,14 @@
+package com.monster.learn;
+
+//20.46%
+public class Fib {
+  public int fib(int N) {
+    if (N == 0) {
+      return 0;
+    }
+    if (N == 1) {
+      return 1;
+    }
+    return fib(N - 1) + fib(N - 2);
+  }
+}
