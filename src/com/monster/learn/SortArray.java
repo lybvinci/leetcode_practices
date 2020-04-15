0d96a9863d5fdf664c3f77930bc6993c1555d32d
liyanbo.monster <liyanbo.monster@bytedance.com>
2020-04-15 10:17:47 +0800

    Finish 912.sortArray

---
+++ src/com/monster/learn/SortArray.java
@@ -0,0 +1,11 @@
+package com.monster.learn;
+
+import java.util.Arrays;
+
+//95.29%
+public class SortArray {
+  public int[] sortArray(int[] nums) {
+    Arrays.sort(nums);
+    return nums;
+  }
+}
