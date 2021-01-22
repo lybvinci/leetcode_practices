09161ca901de9a246c1b8251ef866d07e019e0c3
liyanbo.monster <liyanbo.monster@bytedance.com>
2021-01-22 11:12:46 +0800

    Finish 322.coinChange

--- src/com/monster/learn/CoinChange.java
+++ src/com/monster/learn/CoinChange.java
@@ -29,4 +29,25 @@ public class CoinChange {
         return dp[amount] == 999999 ? -1 : dp[amount];
         */
     }
+
+    //dp[0] = 0;
+    // dp[1] = 1;
+    // dp[2] = min(dp[2],1+dp[i-coin]) ? 
+    //23.38%
+    public int coinChange2(int[] coins, int amount) {
+      int[] dp = new int[amount+1];
+      for (int i = 0; i < dp.length; i++) {
+        dp[i] = amount + 1;
+      }
+      dp[0] = 0;
+      for (int i = 0; i <= amount; i++) {
+        for (int coin : coins) {
+          if (i - coin < 0) {
+            continue;
+          }
+          dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+        }
+      }
+      return dp[amount] == (amount + 1) ? -1 : dp[amount];
+    }
 }
