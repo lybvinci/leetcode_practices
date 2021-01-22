4089a4c6d357241608863344dc896ddd1b3f33e3
liyanbo.monster <liyanbo.monster@bytedance.com>
2021-01-22 10:57:50 +0800

    Finish 509.fib

--- src/com/monster/learn/Fib.java
+++ src/com/monster/learn/Fib.java
@@ -11,4 +11,23 @@ public class Fib {
     }
     return fib(N - 1) + fib(N - 2);
   }
+
+  //100%
+  public int fib2(int N) {
+    if (N == 0) {
+      return 0;
+    }
+    if (N == 1) {
+      return 1;
+    }
+    int pre = 0;
+    int cur = 1;
+    int temp = pre + cur;
+    for (int i = 2; i <= N; i++) {
+      temp = pre + cur;
+      pre = cur;
+      cur = temp;
+    }
+    return cur;
+  }
 }
