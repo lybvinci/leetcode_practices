6a7a83a62f74e79aaaeca8ff12cf27785001829d
liyanbo.monster <liyanbo.monster@bytedance.com>
2021-09-06 20:42:50 +0800

    Finish 1238.circularPermutation. use dfs

--- src/com/monster/learn/CircularPermutation.java
+++ src/com/monster/learn/CircularPermutation.java
@@ -1,6 +1,7 @@
 package com.monster.learn;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 //97.65%
@@ -21,4 +22,41 @@ public class CircularPermutation {
 		}
 		return ret;
 	}	
+
+
+	// 8.24%
+	public List<Integer> f2(int n, int start) {
+		List<Integer> ret = new ArrayList<>();
+		HashSet<Integer> visited = new HashSet<>();
+
+		ret.add(start);
+		visited.add(start);
+		dfs(n, start, ret, visited);
+
+		return ret;
+	}	
+
+	private boolean dfs(int n, int prev, List<Integer> ret, HashSet<Integer> visited) {
+		int size = 1 << n;
+		if (ret.size() == size) {
+			// check is right answer
+			int start = ret.get(0);
+			int end = ret.get(ret.size() - 1);
+			int temp = start ^ end;
+			return (temp & (temp - 1)) == 0;
+		}
+		for (int i = 0; i < n; i++) {
+			int next = prev ^ (1 << i);
+			if (!visited.contains(next)) {
+				visited.add(next);
+				ret.add(next);
+				if (dfs(n, next, ret, visited)) {
+					return true;
+				}
+				visited.remove(next);
+				ret.remove(ret.size() - 1);
+			}
+		}
+		return false;
+	}
 }
