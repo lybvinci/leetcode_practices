28dd8a250bf14b0228da0fd4d9877aa47d1f289f
liyanbo.monster <liyanbo.monster@bytedance.com>
2021-12-13 11:39:00 +0800

    Finish 1504.numSubmat2

--- src/com/monster/learn/NumSubmat.java
+++ src/com/monster/learn/NumSubmat.java
@@ -2,9 +2,9 @@ package com.monster.learn;
 
 import java.util.Stack;
 
-//21.56%
 public class NumSubmat {
 
+    //21.56%
     public int numSubmat(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
@@ -26,6 +26,41 @@ public class NumSubmat {
                 ans += sum;
             }
         }
+        return ans;
+    } 
+
+    //61.08%
+    public int numSubmat2(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[][] temp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    temp[i][j] = mat[i][j];
+                } else if (mat[i][j] == 1) {
+                    temp[i][j] = temp[i][j-1] + 1;
+                } else {
+                    temp[i][j] = 0;
+                }
+            }
+        } 
+
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int width = temp[i][j];
+                for (int k = i; k >= 0; k--) {
+                    width = Math.min(width, temp[k][j]);
+                    ans += width;
+                    if (width == 0) {
+                        break;
+                    }            
+                }
+            }
+        }
+
         return ans;
     } 
     
