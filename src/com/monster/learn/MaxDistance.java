72c36247d546d0fa3e03da692a82e502878cbb23
liyanbo.monster <liyanbo.monster@bytedance.com>
2021-12-23 18:26:28 +0800

    Refactor 2078.maxDistance

--- src/com/monster/learn/MaxDistance.java
+++ src/com/monster/learn/MaxDistance.java
@@ -9,9 +9,9 @@ public class MaxDistance {
     public int maxDistance(int[] colors) {
         int ans = 0;
         for (int i = 0; i < colors.length; i++) {
-            for (int j = 0; j < colors.length; j++) {
+            for (int j = i + 1; j < colors.length; j++) {
                 if (colors[i] != colors[j]) {
-                    ans = Math.max(ans, Math.abs(i - j));
+                    ans = Math.max(ans, j - i);
                 }
             }
         }
