34a464c9d9b4ed534f816762e36cab41857d8565
liyanbo.monster <liyanbo.monster@bytedance.com>
2022-09-01 16:02:42 +0800

    Finish 1636.frequencySort

--- src/com/monster/learn/FrequencySort.java
+++ src/com/monster/learn/FrequencySort.java
@@ -1,9 +1,11 @@
 package com.monster.learn;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -32,5 +34,30 @@ public class FrequencySort {
       }
     }
     return sb.toString();
+  }
+
+  //1636
+  //47.89%
+  public int[] frequencySort(int[] nums) {
+    Integer[] numsInteger = new Integer[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      numsInteger[i] = nums[i];
+    }
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      map.put(nums[i], map.getOrDefault(nums[i], 0) + 1); 
+    }
+    Arrays.sort(numsInteger, new Comparator<Integer>() {
+      @Override
+      public int compare(Integer o1, Integer o2) {
+        int e = map.get(o1).compareTo(map.get(o2));
+        return e == 0 ? o2.compareTo(o1) : e;
+      }
+    });
+    int[] ret = new int[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      ret[i] = numsInteger[i];
+    }
+    return ret;
   }
 }
